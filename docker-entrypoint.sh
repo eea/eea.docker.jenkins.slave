a0ba6e944612d9678fee167ba3d8e8b15c919303
valentinab25 <30239069+valentinab25@users.noreply.github.com>
2024-08-23 12:17:32 +0300

    add git error fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -125,6 +125,9 @@ cp /root/.ssh/known_hosts /var/jenkins_home/worker/.ssh/known_hosts
 chmod 644 /var/jenkins_home/worker/.ssh/known_hosts
 chown -R jenkins:jenkins /var/jenkins_home/worker/.ssh
 
+#do not check user permissions on directories
+git config --global --add safe.directory '*'
+git config --system --add safe.directory '*'
 
 if [ "$1" = "java" ]; then
   exec gosu $RUN_AS_USER java $JAVA_OPTS -jar /bin/swarm-client.jar -fsroot /var/jenkins_home/worker/ -labels "$JENKINS_LABELS" $PARAMS
